aa23c63a500fbe4d793f3c5da85a3a401e18e052
Damian Yordanov <dyordanov@asteasolutions.com>
2016-11-08 19:50:02 +0200

    Adds width option to hexagon

--- scripts/directives/hexagon.js
+++ scripts/directives/hexagon.js
@@ -2,6 +2,27 @@
 
 angular.module('schrodinger').directive('hexagon', function () {
     return {
-        templateUrl: 'views/hexagon.html'
+        templateUrl: 'views/hexagon.html',
+        scope: {
+            width: '='
+        },
+        link: function ($scope, $element) {
+            var top = angular.element($element[0].querySelector('.top')),
+                middle = angular.element($element[0].querySelector('.middle')),
+                bottom = angular.element($element[0].querySelector('.bottom'));
+
+            $scope.$watch('width', function () {
+                top.css('border-bottom-width', Math.round($scope.width / 2 / Math.sqrt(3)));
+                top.css('border-left-width', Math.round($scope.width / 2));
+                top.css('border-right-width', Math.round($scope.width / 2));
+
+                middle.css('width', $scope.width);
+                middle.css('height', Math.round($scope.width / Math.sqrt(3)));
+
+                bottom.css('border-top-width', Math.round($scope.width / 2 / Math.sqrt(3)));
+                bottom.css('border-right-width', Math.round($scope.width / 2));
+                bottom.css('border-left-width', Math.round($scope.width / 2));
+            });
+        }
     };
 });
